7da0f6ea47b8a3cdba3fd764a469486724154295
VJ-Varanasi <108487086+VJ-Varanasi@users.noreply.github.com>
2025-11-03 10:59:52 -0500

    Defining A1 as a variable

--- theoretical_estimate_helper.py
+++ theoretical_estimate_helper.py
@@ -10,14 +10,14 @@ def log_sum (vals):
         return 0
 
 # Estimates size of |B_k| (eq 11)
-def raf_term(k,mR, Q, k_prime = 163, n= 6 ):
+def raf_term(k,mR, Q, k_prime = 163, n= 6, A1= 90 ):
     C = Q * np.log(k_prime) - 2* n * k_prime *np.log(2)
     #bks = 0
     bks = []
 
     for m in range(1, k): 
         part1 = rest_part(Q,m, C)
-        singles = wiki_approx(mR - 90 - m, k- m)
+        singles = wiki_approx(mR - A1 - m, k- m)
 
         
         correction = rest_part(Q,k-m, C)
